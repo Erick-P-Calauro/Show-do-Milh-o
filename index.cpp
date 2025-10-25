d3fc2aef05007b17189ef18b8be59465c56f9b09
Erick-P-Calauro <erickcalauro@gmail.com>
2025-10-25 14:07:01 -0400

    feat: Recursos adicionados : Pular pergunta, respostas da plateia e dos universitarios.

--- index.cpp
+++ index.cpp
@@ -9,17 +9,24 @@ int main() {
 
     float valor_acumulado = 0;
     int sorteio_pergunta = 0;
+    
     // 1 a 16
     int pergunta_count = 1; 
+    int perguntas_realizadas[5] = {0, 0, 0, 0, 0};
+
     // 0,1,2,3 (Define as dificuldades) 
     int bloco = 0;  
+    
     // Pular, Pedir Ajuda da Platéia, Pedir Ajuda dos Universitários, Pedir Ajuda Cartas;
     int recursos[] = {3, 3, 3, 3};  
-    int perguntas_realizadas[5] = {0, 0, 0, 0, 0};
+    
+    // Alternativa Escolhida ([1,2,3,4,5 - a,b,c,d])
     char escolha = 0;
-    int continuar = 1;
 
-    srand(time(NULL));
+    // Flags para continuidade do jogo e passagem para proxima pergunta
+    int continuar = 1, resposta_valida = 1; 
+
+    srand(time(0));
 
     while(continuar) {
 
@@ -36,76 +43,135 @@ int main() {
 
         Pergunta pergunta = ler_pergunta(sorteio_pergunta, bloco);
 
-        mostrar_pergunta(pergunta, pergunta_count, valor_acumulado, recursos);
+        // Loop do while para verificar a validade da resposta
+        // Se for selecionados recursos já utilizados a resposta é inválida
+        // A invalidez da resposta mostra a mesma pergunta novamente
+        do{ 
+            resposta_valida = 1;
 
-        printf("Resposta : \n");
-        scanf("%c%*c", &escolha);
+            mostrar_pergunta(pergunta, pergunta_count, valor_acumulado, recursos);
+            printf("Resposta : \n");
+            scanf("%c%*c", &escolha);
 
-        switch(escolha) {
-            case '1':
-                break;
-            case '2':
-                break;
-            case '3':
-                break;
-            case '4':
-                break;
-            case '5':
-                printf("\nVoce saiu com R$ %.2f\n\n", valor_acumulado);
-                continuar = 0;
-                break;
-            case 'a':
-            case 'b':
-            case 'c':
-            case 'd':
-                
-                if(escolha == pergunta.alt_correta) {
-                    
-                    if(pergunta_count == 16) {
-                        printf("\n\nParabens, voce foi o grande vencedor do show do milhao !!\n\n");
-                        valor_acumulado += 400000;
-                        printf("Valor acumulado : R$ %.2f\n\n", valor_acumulado);
-                        continuar = 0;
+            switch(escolha) {
+                case '1':
+
+                    // Se tem recurso[0] (Pular) pula a pergunta
+                    if(recursos[0] == 0) {
+                        resposta_valida = 0;
                     }else {
-                        printf("\nAlternativa Correta !!\n");
-                        printf("Voce ganhou R$ %.2f\n", pow(10, 3 + bloco));
-                        valor_acumulado += pow(10, 3 + bloco);
+                        recursos[0]--;
                     }
 
-                }else {
-                    printf("\nAlternativa escolhida : %c\n", escolha);
-                    printf("Alternativa Correta : %c\n", pergunta.alt_correta);
-                    printf("Voce foi eliminado\n");
-                    printf("Valor acumulado perdido : R$ %.2f\n\n", valor_acumulado);
+                    break;
+                case '2':
+
+                    resposta_valida = 0;
+
+                    if(recursos[1] != 0) {
+                        
+                        int contagem_plateia[] = {0,0,0,0}; // a,b,c,d
+                        int *probabilidades = criar_array_plateia(pergunta.alt_correta);
+
+                        // Fazendo o sorteio das 30 perguntas com base no array probabilidades
+                        for(int i = 0; i < 30; i++) {
+                            contagem_plateia[probabilidades[rand() % 10]]++;
+                        }
+
+                        printf("Respostas da Plateia : \n");
+                        printf("A : %d - B : %d - C : %d - D : %d\n", contagem_plateia[0], contagem_plateia[1], contagem_plateia[2], contagem_plateia[3]);
+
+                        recursos[1]--;
+                        free(probabilidades);
+                    }
+                
+                    break;
+                case '3':
+
+                    resposta_valida = 0;
+
+                    if(recursos[2] != 0) {
+                        char contagem_universitarios[] = {'\0', '\0', '\0'};
+                        int *probabilidades = criar_array_universitarios(pergunta.alt_correta);
+
+                        // Atribuindo as respostas dos universitarios com base na probabilidade
+                        // A soma de 97 se deve aos pormenores do array de probabilidades (Ver utilidades.cpp)
+                        for(int i = 0; i < 3; i++) {
+                            contagem_universitarios[i] = probabilidades[rand() % 10] + 97;
+                        }
+
+                        printf("Respostas dos Universitarios : \n");
+                        printf("Universitario 1 : %c\nUniversitario 2 : %c\nUniversitario 3 : %c\n", contagem_universitarios[0], contagem_universitarios[1], contagem_universitarios[2]);
+                        
+                        recursos[2]--;
+                        free(probabilidades);
+                    }
+
+                    break;
+                case '4':
+                    break;
+                case '5':
+                    printf("\nVoce saiu com R$ %.2f\n\n", valor_acumulado);
                     continuar = 0;
-                }
+                    break;
+                case 'a':
+                case 'b':
+                case 'c':
+                case 'd':
+                    
+                    // Lógica acerca das respostas
+                    if(escolha == pergunta.alt_correta) {
+                        
+                        if(pergunta_count == 16) {
+                            printf("\n\nParabens, voce foi o grande vencedor do show do milhao !!\n\n");
+                            valor_acumulado += 400000;
+                            printf("Valor acumulado : R$ %.2f\n\n", valor_acumulado);
+                            continuar = 0;
+                        }else {
+                            printf("\nAlternativa Correta !!\n");
+                            printf("Voce ganhou R$ %.2f\n", pow(10, 3 + bloco));
+                            valor_acumulado += pow(10, 3 + bloco);
+                        }
 
-                break;
-            default:
-                printf("Alternativa invalida...");
-                break;
-        }
+                    }else {
+                        printf("\nAlternativa escolhida : %c\n", escolha);
+                        printf("Alternativa Correta : %c\n", pergunta.alt_correta);
+                        printf("Voce foi eliminado.\n");
+                        printf("Valor acumulado perdido : R$ %.2f\n\n", valor_acumulado);
+                        continuar = 0;
+                    }
 
-        // Realiza mudança das dificuldades
-        if(pergunta_count % 5 == 0 && continuar) {
-            bloco += 1;
-            resetar_vetor(perguntas_realizadas, 5);
+                    // Realiza mudança das dificuldades
+                    if(pergunta_count % 5 == 0 && continuar) {
+                        printf("%d\n", bloco);
 
-            if(bloco == 1) {
-                printf("Passou para o bloco de dificuldade 2 e ganhou R$ 5000\n");
-                valor_acumulado += 5000;
+                        bloco += 1;
+                        resetar_vetor(perguntas_realizadas, 5);
 
-            }else if(bloco == 2) {
-                printf("Passou para o bloco de dificuldade 3 e ganhou R$ 40000\n");
-                valor_acumulado += 40000;
+                        if(bloco == 1) {
+                            printf("Passou para o bloco de dificuldade 2 e ganhou R$ 5000\n");
+                            valor_acumulado += 5000;
 
-            }else if(bloco == 3) {
-                printf("Passou para a ultima pergunta !");
+                        }else if(bloco == 2) {
+                            printf("Passou para o bloco de dificuldade 3 e ganhou R$ 40000\n");
+                            valor_acumulado += 40000;
+
+                        }else if(bloco == 3) {
+                            printf("Passou para a ultima pergunta !");
+                        }
+
+                    }
+
+                    pergunta_count++; 
+
+                    break;
+                default:
+                    printf("Alternativa invalida...\n");
+                    resposta_valida = 0;
+                    break;
             }
 
-        }
-        
-        pergunta_count++;
+        }while(!resposta_valida);
     }
 
     return 0;
--- utilidades.cpp
+++ utilidades.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 bool inteiro_em_vetor(int valor, int* vetor);
 void resetar_vetor(int* vetor, size_t tam);
+int* criar_array_probabilidades(char alt_correta);
 
 bool inteiro_em_vetor(int valor, int* vetor, size_t tam) {
     
@@ -18,4 +20,52 @@ void resetar_vetor(int* vetor, size_t tam) {
     for(int i = 0; i < tam; i++) {
         vetor[i] = 0;
     }
+}
+
+int* criar_array_plateia(char alt_correta) {
+    const char alternativas[] = {'a', 'b', 'c', 'd'};
+    int count = 3;
+    int *probabilidades = (int *) malloc(sizeof(int) * 10);
+
+    // 4/10 alternativas certas
+    for(int i = 0; i < 4; i++) {
+        probabilidades[i] = alt_correta % 97;
+    }
+
+    // 2/10 cada alternativa errada
+    for(int i = 0; i < 4; i++) {
+        if(alternativas[i] != alt_correta) {
+            probabilidades[count+1] = alternativas[i] % 97;
+            probabilidades[count+2] = alternativas[i] % 97;
+            count += 2;
+        }
+        
+    }
+
+    // Array com 4/10 certo e 6/10 errado.
+    return probabilidades;
+}
+
+int* criar_array_universitarios(char alt_correta) {
+    const char alternativas[] = {'a', 'b', 'c', 'd'};
+    int count = 6;
+    int *probabilidades = (int *) malloc(sizeof(int) * 10);
+
+    // 7/10 alternativas certas
+    for(int i = 0; i < 7; i++) {
+        probabilidades[i] = alt_correta % 97;
+    }
+
+    // 1/10 cada alternativa errada
+    for(int i = 0; i < 4; i++) {
+        if(alternativas[i] != alt_correta) {
+            probabilidades[count+1] = alternativas[i] % 97;
+            count++;
+        }
+        
+    }
+
+    // Array com 4/10 certo e 6/10 errado.
+    return probabilidades;
+
 }
